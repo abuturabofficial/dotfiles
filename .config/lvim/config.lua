c835ca93b6aeedaf7c04b381721f4974f5762081
Sajid Mahmood <sajidmahmood.dev@gmail.com>
2022-09-05 14:55:58 +0500

    fix and add formatter for python

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -73,7 +73,7 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -142,11 +142,11 @@ require("lvim.lsp.manager").setup("ltex")
 -- end
 
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "black", filetypes = { "python" } },
+ local formatters = require "lvim.lsp.null-ls.formatters"
+ formatters.setup {
+   { command = "black", filetypes = { "python" } },
 --   { command = "isort", filetypes = { "python" } },
---   {
+}
 --     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
 --     command = "prettier",
 --     ---@usage arguments to pass to the formatter
